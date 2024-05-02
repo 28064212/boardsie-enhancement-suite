e5fb56f002ddc233f52adf31ccb71b5e56cfbd12
28064212 <28064212.dev@gmail.com>
2024-05-02 15:40:20 +0100

    v0.3.3

--- src/js/boardsie-enhancement-suite.js
+++ src/js/boardsie-enhancement-suite.js
@@ -3,7 +3,7 @@ let showpreviews = false;
 let settings = {};
 let userDetails;
 let api = "https://www.boards.ie/api/v2/";
-const version = '0.3.2';
+const version = '0.3.3';
 
 if (window.top == window.self) {
 	try {
